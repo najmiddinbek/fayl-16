0b74c1e3b5cedf6d7ae1c8254492809a8db940b7
najmiddin <turgunpolatov3gmail.com>
2023-11-03 02:11:28 +0500

    commit

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -6,12 +6,9 @@ export default function Navbar() {
     return (
         <div className="green">
             <div className="max-w-[1400px]  mx-auto">
-                <nav className="flex justify-between items-center">
+                <nav className="flex justify-center items-center">
                     <Link href={"/pupilsAdd"}>
                         <Image src={Logotip} width={100} height={100} alt="Image" />
-                    </Link>
-                    <Link className="text-white" href={"/pupilsAdd"}>
-                        O`quvchi qo`shish
                     </Link>
                 </nav>
             </div>
